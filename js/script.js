81da00ed1f789d3f736e4403b67c627e0e4395db
lindam <linda.minotti2@gmail.com>
2020-05-27 09:16:51 +0200

    Fatto anche con ciclo while

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 // Chiedi all’utente il cognome
 var cognomeUtente = prompt('Scrivi cognome da inserire in lista con prima lettera maiuscola');
-// console.log('cognome inserito: ' + cognomeUtente);
 
 // Crea lista cognomi
 var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
-console.log(listaCognomi);
-// console.log(listaCognomi[3]);
 
 // Aggiungi cognome dell'utente alla lista
 listaCognomi.push(cognomeUtente);
-console.log(listaCognomi);
 
 // Ordina la lista in ordine alfabetico
 listaCognomi.sort();
-console.log(listaCognomi);
-// Stampa la lista ordinata
-for (i = 0; i < listaCognomi.length; i++) {
+
+// // Stampa la lista ordinata: ciclo for
+// for (i = 0; i < listaCognomi.length; i++) {
+//   var listaCognomiStampa = document.getElementById('stampa_lista');
+//   var contenutoAttuale = listaCognomiStampa.innerHTML;
+//   listaCognomiStampa.innerHTML = contenutoAttuale + '<li>' + listaCognomi[i] + '</li>';
+// }
+
+// Stampa lista ordinata: ciclo while
+var i = 0;
+while (i < listaCognomi.length) {
   var listaCognomiStampa = document.getElementById('stampa_lista');
   var contenutoAttuale = listaCognomiStampa.innerHTML;
-  listaCognomiStampa.innerHTML = contenutoAttuale + '<li>' + listaCognomi[i] + '</li>'
+  listaCognomiStampa.innerHTML = contenutoAttuale + '<li>' + listaCognomi[i] + '</li>';
+  i++;
 }
-
 // Scrivi anche la posizione della lista in cui il nuovo cognome si trova
 var newPositionValue = listaCognomi.indexOf(cognomeUtente) + 1;
 var newPosition = document.getElementById('new_position');
